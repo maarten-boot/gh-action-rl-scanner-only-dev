91d6be35bb83fcc79db550d51a65f97ba6786d67
Maarten Boot <11140490+maarten-boot@users.noreply.github.com>
2023-09-29 10:49:11 +0200

    script

--- run-scanner.sh
+++ run-scanner.sh
@@ -1,11 +1,23 @@
-#! /usr/bin/env bash
+#! /bin/bash
+
+do_verbose()
+{
+    cat <<!
+REPORT_PATH:              ${REPORT_PATH}
+MY_ARTIFACT_TO_SCAN_PATH: ${MY_ARTIFACT_TO_SCAN_PATH}
+RL_STORE:                 ${RL_STORE}
+RL_PACKAGE_URL:           ${RL_PACKAGE_URL}
+RL_DIFF_WITH:             ${RL_DIFF_WITH}
+RL_VERBOSE:               ${RL_VERBOSE}
+!
+}
 
 prep_report()
 {
     if [ -z "${REPORT_PATH}" ]
     then
-      echo "FATAL: no report path provided" >&2
-      exit 101
+        echo "FATAL: no report path provided" >&2
+    exit 101
     fi
 
     rm -rf "${REPORT_PATH}"
@@ -22,20 +34,20 @@ prep_paths()
 
 scan_no_store()
 {
-    # docker pull reversinglabs/rl-scanner:latest
     docker run --rm -u $(id -u):$(id -g) \
-      -e "RLSECURE_ENCODED_LICENSE=${RLSECURE_ENCODED_LICENSE}" \
-      -e "RLSECURE_SITE_KEY=${RLSECURE_SITE_KEY}" \
-      -v "${A_DIR}/:/packages:ro" \
-      -v "${R_PATH}/:/report" \
-      reversinglabs/rl-scanner:latest \
+    -e "RLSECURE_ENCODED_LICENSE=${RLSECURE_ENCODED_LICENSE}" \
+    -e "RLSECURE_SITE_KEY=${RLSECURE_SITE_KEY}" \
+    -v "${A_DIR}/:/packages:ro" \
+    -v "${R_PATH}/:/report" \
+    reversinglabs/rl-scanner:latest \
         rl-scan --package-path="/packages/${A_FILE}" \
-          --report-path=/report \
-          --report-format=all}
+            --report-path=/report \
+            --report-format=all}
 }
 
 main()
 {
+    do_verbose
     prep_report
     prep_paths
     scan_no_store
