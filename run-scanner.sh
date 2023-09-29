84d914c92f5f52b1d5bf9d1bc23a4ca7deecf533
Maarten Boot <11140490+maarten-boot@users.noreply.github.com>
2023-09-29 10:20:38 +0200

    fix env

--- run-scanner.sh
+++ run-scanner.sh
@@ -2,20 +2,20 @@
 
 prep_report()
 {
-    if [ -z "${{ inputs.report-path }}" ]
+    if [ -z "${REPORT_PATH}" ]
     then
       echo "FATAL: no report path provided" >&2
       exit 101
     fi
 
-    rm -rf "${{ inputs.report-path }}"
-    mkdir -p "${{ inputs.report-path }}"
+    rm -rf "${REPORT_PATH}"
+    mkdir -p "${REPORT_PATH}"
 }
 
 prep_paths()
 {
-    R_PATH=$( realpath "${{ inputs.report-path }}" )
-    A_PATH=$( realpath "${{ inputs.artifact-to-scan }}" )
+    R_PATH=$( realpath "${REPORT_PATH}" )
+    A_PATH=$( realpath "${MY_ARTIFACT_TO_SCAN_PATH}" )
     A_DIR=$( dirname "${A_PATH}" )
     A_FILE=$( basename "${A_PATH}" )
 }
@@ -24,14 +24,14 @@ scan_no_store()
 {
     # docker pull reversinglabs/rl-scanner:latest
     docker run --rm -u $(id -u):$(id -g) \
-      -e "RLSECURE_ENCODED_LICENSE=${{ env.RLSECURE_ENCODED_LICENSE }}" \
-      -e "RLSECURE_SITE_KEY=${{ env.RLSECURE_SITE_KEY }}" \
+      -e "RLSECURE_ENCODED_LICENSE=${RLSECURE_ENCODED_LICENSE}" \
+      -e "RLSECURE_SITE_KEY=${RLSECURE_SITE_KEY}" \
       -v "${A_DIR}/:/packages:ro" \
       -v "${R_PATH}/:/report" \
       reversinglabs/rl-scanner:latest \
         rl-scan --package-path="/packages/${A_FILE}" \
           --report-path=/report \
-          --report-format=all
+          --report-format=all}
 }
 
 main()
